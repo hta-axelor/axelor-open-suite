b030f1a4dbb42e3d93cebab2c688ce32757dba39
hta-axelor <hta.axelor@gmail.com>
2020-06-12 19:02:06 +0530

    Refactor code

--- axelor-project/src/main/java/com/axelor/apps/project/event/ProjectActivityEvent.java
+++ axelor-project/src/main/java/com/axelor/apps/project/event/ProjectActivityEvent.java
@@ -7,6 +7,8 @@ import com.axelor.apps.project.db.repo.ProjectRepository;
 import com.axelor.apps.project.db.repo.TopicRepository;
 import com.axelor.apps.project.db.repo.WikiRepository;
 import com.axelor.apps.project.service.ProjectActivityService;
+import com.axelor.db.JPA;
+import com.axelor.db.Model;
 import com.axelor.event.Observes;
 import com.axelor.events.PostRequest;
 import com.axelor.events.RequestEvent;
@@ -20,10 +22,6 @@ import javax.inject.Named;
 public class ProjectActivityEvent {
 
   private ProjectActivityService projectActivityService;
-  private ProjectRepository projectRepo;
-  private TeamTaskRepository teamTaskRepo;
-  private WikiRepository wikiRepo;
-  private TopicRepository topicRepo;
 
   @Inject
   public ProjectActivityEvent(
@@ -33,57 +31,48 @@ public class ProjectActivityEvent {
       WikiRepository wikiRepo,
       TopicRepository topicRepo) {
     this.projectActivityService = projectActivityService;
-    this.projectRepo = projectRepo;
-    this.teamTaskRepo = teamTaskRepo;
-    this.wikiRepo = wikiRepo;
-    this.topicRepo = topicRepo;
   }
 
   public void onSaveProject(
       @Observes @Named(RequestEvent.SAVE) @EntityType(Project.class) PostRequest event) {
-    Map<String, Object> dataMap = event.getRequest().getData();
-    if (dataMap != null) {
-      Object id = dataMap.get("id");
-      if (id != null) {
-        Project project = projectRepo.find(Long.parseLong(id.toString()));
-        projectActivityService.getProjectActivity(project);
-      }
+    Project project = getRecord(event, Project.class);
+    if (project != null) {
+      projectActivityService.getProjectActivity(project);
     }
   }
 
   public void onSaveTask(
       @Observes @Named(RequestEvent.SAVE) @EntityType(TeamTask.class) PostRequest event) {
-    Map<String, Object> dataMap = event.getRequest().getData();
-    if (dataMap != null) {
-      Object id = dataMap.get("id");
-      if (id != null) {
-        TeamTask task = teamTaskRepo.find(Long.parseLong(id.toString()));
-        projectActivityService.getProjectActivity(task);
-      }
+    TeamTask task = getRecord(event, TeamTask.class);
+    if (task != null) {
+      projectActivityService.getProjectActivity(task);
     }
   }
 
   public void onSaveWiki(
       @Observes @Named(RequestEvent.SAVE) @EntityType(Wiki.class) PostRequest event) {
-    Map<String, Object> dataMap = event.getRequest().getData();
-    if (dataMap != null) {
-      Object id = dataMap.get("id");
-      if (id != null) {
-        Wiki wiki = wikiRepo.find(Long.parseLong(id.toString()));
-        projectActivityService.getProjectActivity(wiki);
-      }
+    Wiki wiki = getRecord(event, Wiki.class);
+    if (wiki != null) {
+      projectActivityService.getProjectActivity(wiki);
     }
   }
 
   public void onSaveTopic(
       @Observes @Named(RequestEvent.SAVE) @EntityType(Topic.class) PostRequest event) {
+    Topic topic = getRecord(event, Topic.class);
+    if (topic != null) {
+      projectActivityService.getProjectActivity(topic);
+    }
+  }
+
+  private <T extends Model> T getRecord(RequestEvent event, Class<T> klass) {
     Map<String, Object> dataMap = event.getRequest().getData();
     if (dataMap != null) {
       Object id = dataMap.get("id");
       if (id != null) {
-        Topic topic = topicRepo.find(Long.parseLong(id.toString()));
-        projectActivityService.getProjectActivity(topic);
+        return JPA.find(klass, Long.parseLong(id.toString()));
       }
     }
+    return null;
   }
 }
