3cd2cb5ed07ba8f765aea6bcd939b13775b44cae
cma-axelor <c.malaviya@axelor.com>
2020-07-21 11:21:14 +0530

    Task editor:add task editor

---
+++ axelor-project/src/main/webapp/project/task-editor/precache-manifest.7580bcf84e5741c4c9937591e55d8f8b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efe88ca332368870a82cc9d75b3be152",
+    "url": "./index.html"
+  },
+  {
+    "revision": "57212144036a54f0f812",
+    "url": "./static/css/2.0b510fe8.chunk.css"
+  },
+  {
+    "revision": "4fed36c23c4cf3634773",
+    "url": "./static/css/main.2247e7e8.chunk.css"
+  },
+  {
+    "revision": "57212144036a54f0f812",
+    "url": "./static/js/2.284f8f04.chunk.js"
+  },
+  {
+    "revision": "5a734cd1df47a9406c6392f11fd44928",
+    "url": "./static/js/2.284f8f04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fed36c23c4cf3634773",
+    "url": "./static/js/main.0450db66.chunk.js"
+  },
+  {
+    "revision": "083e6c14fe78135d6547",
+    "url": "./static/js/runtime-main.298acfe2.js"
+  },
+  {
+    "revision": "5d75554dbc1274af3ce8f98a3ca41a50",
+    "url": "./static/media/user.5d75554d.jpg"
+  }
+]);
