797231838f3245ab34b3e06dec709415b515e3bc
cma-axelor <c.malaviya@axelor.com>
2020-07-23 11:55:36 +0530

    Fix:Kanban column filter

--- axelor-project/src/main/java/com/axelor/apps/project/web/ProjectController.java
+++ axelor-project/src/main/java/com/axelor/apps/project/web/ProjectController.java
@@ -18,14 +18,22 @@
 package com.axelor.apps.project.web;
 
 import com.axelor.apps.project.db.Project;
+import com.axelor.apps.project.db.ProjectStatus;
+import com.axelor.apps.project.db.repo.ProjectStatusRepository;
 import com.axelor.apps.project.service.ProjectService;
+import com.axelor.common.ObjectUtils;
+import com.axelor.i18n.I18n;
 import com.axelor.inject.Beans;
+import com.axelor.meta.schema.actions.ActionView;
+import com.axelor.meta.schema.actions.ActionView.ActionViewBuilder;
 import com.axelor.rpc.ActionRequest;
 import com.axelor.rpc.ActionResponse;
+import com.axelor.team.db.TeamTask;
 import com.axelor.team.db.repo.TeamTaskRepository;
 import com.google.inject.Singleton;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @Singleton
 public class ProjectController {
@@ -90,5 +98,36 @@ public class ProjectController {
             .getTaskView(
                 "All tasks", "self.typeSelect = :typeSelect AND self.project = :_project", context);
     response.setView(view);
+  }
+
+  public void perStatusKanban(ActionRequest request, ActionResponse response) {
+    Project project = request.getContext().asType(Project.class);
+    Map<String, Object> context = new HashMap<>();
+    context.put("_project", project);
+    context.put("typeSelect", TeamTaskRepository.TYPE_TASK);
+
+    String statusColumnsTobeExcluded =
+        Beans.get(ProjectStatusRepository.class)
+            .all()
+            .filter("self not in :allowedTeamTaskStatus")
+            .bind("allowedTeamTaskStatus", project.getTeamTaskStatusSet())
+            .fetchStream()
+            .map(ProjectStatus::getId)
+            .map(String::valueOf)
+            .collect(Collectors.joining(","));
+
+    ActionViewBuilder builder =
+        ActionView.define(I18n.get("All tasks"))
+            .model(TeamTask.class.getName())
+            .add("kanban", "team-task-kanban")
+            .add("grid", "team-task-grid")
+            .add("form", "team-task-form")
+            .domain("self.typeSelect = :typeSelect AND self.project = :_project")
+            .param("kanban-hide-columns", statusColumnsTobeExcluded);
+
+    if (ObjectUtils.notEmpty(context)) {
+      context.forEach(builder::context);
+    }
+    response.setView(builder.map());
   }
 }
